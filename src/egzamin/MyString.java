f19157a2d6e1bb245015649c1cae4c9bb77d9d21
mpolejewski <mpolejewski@PC527.mir.local>
2017-09-26 13:57:25 +0200

    2017_09_26

---
+++ src/egzamin/MyString.java
@@ -0,0 +1,11 @@
+package egzamin;
+
+public class MyString {
+	String msg;
+	MyString(String msg) {
+		this.msg = msg;
+		
+	}
+	
+
+}
---
+++ src/egzamin/X.java
@@ -0,0 +1,51 @@
+package egzamin;
+
+class X {
+
+//	static int i;
+//	public int j;
+	
+//	private static int i;
+
+	public static void main(String[] args) {
+//		
+//		int ii =0;
+//		int jj =7;
+//		
+//		for (ii =0; ii< jj-3; ii =ii+2) {
+//			System.out.print(ii + " ");
+//		}
+//		
+//		X x1 = new X();
+//		
+//		X x2 = new X();
+//				
+//		x1.i =3;
+//		x1.j = 4;
+//		x2.i = 5;
+//		x2.j = 6;
+//		
+//		System.out.println(
+//		x1.i + " " +
+//		x1.j + " " +
+//				x2.i + " " +
+//		x2.j);
+		
+		
+//		array[0] =10;
+//		array[1]=20;
+		
+//		String[] arr = {"A", "B", "C", "D"};
+//		for(int i =0; i<arr.length; i++); {
+//			System.out.print(arr[i] + " ");
+//		if (arr[i].equals("C")) {
+//					continue;
+//				}
+//				
+		System.out.println("5 + 2 = " + 3+4);
+		
+		System.out.println("5 + 2 = " + (3+4));
+//
+//	}
+		
+	}}
